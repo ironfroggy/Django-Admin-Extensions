37115413e49d8d6b65d15f1caad0ff359d8e29f7
Calvin Spealman <ironfroggy@gmail.com>
2011-01-28 14:25:53 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-admin-extensions',
+    description = 'Tools for extending existing Django admin classes.',
+    author = 'Calvin Spealman',
+    author_email = 'ironfroggy@gmail.com',
+
+    py_modules = ['djadminext'],
+)
